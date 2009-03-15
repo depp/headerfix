29e2af4b2fa2b2dea8d8c09b133eebbfd1b92b0c
Dietrich Epp <depp@zdome.net>
2009-03-15 13:43:15 -0700

    Wrote setup script for header fixer.

---
+++ setup.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3.0
+from distutils.core import setup
+setup(name='Fixheader',
+      version='1.0',
+      description='Source code header fixer',
+      author='Dietrich Epp',
+      author_email='depp@zdome.net',
+      py_modules=['fixheader'])
