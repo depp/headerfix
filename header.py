475ec63d375f115c5ffecb304374703212b9554e
Dietrich Epp <depp@zdome.net>
2009-03-15 13:39:35 -0700

    Updated header script to separate it from the details of a project.

---
+++ header.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3.0
+# Header Fixer - header.py
+# Copyright 2007 - 2009 Dietrich Epp <depp@zdome.net>
+# This source code is licensed under the GNU General Public License,
+# Version 3. See gpl-3.0.txt for details.
+import fixheader
+
+LICENSE = [
+    'This source code is licensed under the GNU General Public License,',
+    'Version 3. See gpl-3.0.txt for details.'
+]
+
+class MyHeaderFixer(fixheader.HeaderFixer):
+    def project_name(self, path):
+        return "Header Fixer"
+    def project_author(self, path):
+        return "Dietrich Epp <depp@zdome.net>"
+    def header_suffix(self, path):
+        return LICENSE
+
+MyHeaderFixer().run()
